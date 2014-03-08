83160514370a90bc1a1ba41d163faa5fd39da1fa
Kostya <flashfoxter@mail.ru>
2014-03-09 02:36:28 +0400

    Init

---
+++ RndMongoIdentity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RndMongoIdentity.MvcApplication" Language="C#" %>
---
+++ RndMongoIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RndMongoIdentity.Startup))]
+namespace RndMongoIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
